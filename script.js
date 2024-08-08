284fbb0ee7dca8418a4f0b0be38d35efe8e26316
Amina do Couto <146983144+aminacouto@users.noreply.github.com>
2024-08-08 12:02:04 -0300

    scroll

---
+++ script.js
@@ -0,0 +1,28 @@
+window.sr = ScrollReveal ({reset: true});
+
+sr.reveal('.inicio', {duration: 2000});
+
+sr.reveal('.sobre', {
+    duration: 2000,
+    rotate: {x:30, y:0, z:0}
+});
+
+sr.reveal('.formacao', {
+    duration: 2000,
+    rotate: {x:30, y:0, z:0}
+});
+
+sr.reveal('.idiomas', {
+    duration: 2000,
+    rotate: {x:30, y:0, z:0}
+});
+
+sr.reveal('.portfolio', {
+    duration: 2000,
+    rotate: {x:30, y:0, z:0}
+});
+
+sr.reveal('.contato', {
+    duration: 2000,
+    rotate: {x:30, y:0, z:0}
+});
