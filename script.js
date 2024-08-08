a05dadc995f14c66534d4b16bdd7fcaecb8a5c24
Amina do Couto <146983144+aminacouto@users.noreply.github.com>
2024-08-08 15:53:18 -0300

    ajustes transição

--- script.js
+++ script.js
@@ -3,26 +3,26 @@ window.sr = ScrollReveal ({reset: true});
 sr.reveal('.inicio', {duration: 2000});
 
 sr.reveal('.sobre', {
-    duration: 2000,
-    rotate: {x:30, y:0, z:0}
+    duration: 2500,
+    rotate: {x:0, y:0, z:0}
 });
 
 sr.reveal('.formacao', {
-    duration: 2000,
-    rotate: {x:30, y:0, z:0}
+    duration: 2500,
+    rotate: {x:0, y:0, z:0}
 });
 
 sr.reveal('.idiomas', {
-    duration: 2000,
-    rotate: {x:30, y:0, z:0}
+    duration: 2500,
+    rotate: {x:0, y:0, z:0}
 });
 
 sr.reveal('.portfolio', {
-    duration: 2000,
-    rotate: {x:30, y:0, z:0}
+    duration: 3000,
+    rotate: {x:80, y:0, z:0}
 });
 
 sr.reveal('.contato', {
-    duration: 2000,
-    rotate: {x:30, y:0, z:0}
+    duration: 3000,
+    rotate: {x:0, y:90, z:0}
 });
