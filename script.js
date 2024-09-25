d79bb1dc44799f347b5b9d1890917186a4db65a1
Amina do Couto <146983144+aminacouto@users.noreply.github.com>
2024-09-25 16:49:54 -0300

    estilo menu

--- script.js
+++ script.js
@@ -25,3 +25,12 @@ sr.reveal('.portfolio', {
     duration: 3000,
     rotate: {x:80, y:0, z:0}
 });
+
+function abrirMenu() {
+    const menu = document.getElementById('menu');
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+}
